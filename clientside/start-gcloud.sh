693ec53564d028af6b01e73f33807d1779cd9418
Stefan Baur <X2Go-ML-1@baur-itcs.de>
2024-09-05 21:25:25 +0200

    reworked and fixed mounts (now mounting GCS-/ instead of GCS-homedir) and file paths, fixed use # instead of / for filename substitution in sessions file when using sed once more

--- clientside/start-gcloud.sh
+++ clientside/start-gcloud.sh
@@ -19,6 +19,7 @@ SSH_IP=$(echo -e "$CLOUD_OUTPUT" | tr ':@' '  ' | awk '$1=="sshfs" {print $3 }')
 SSH_PORT=$(echo -e "$CLOUD_OUTPUT" | awk '$1=="sshfs" {print $5 }')
 SSH_KEYFILE=$(echo -e "$CLOUD_OUTPUT" | tr '=' ' ' | awk '$1=="sshfs" {print $7 }')
 SSH_OPTIONS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+CLOUD_OUTPUT=$(echo $CLOUD_OUTPUT | sed -e "s#$SSH_IP:#$SSH_IP:/#")
 
 if [ -z "$SSH_AGENT_PID" ] || [ -z "$SSH_AUTH_SOCK" ] ; then
 	eval $(ssh-agent)
@@ -28,14 +29,14 @@ if ! ssh-add -l | grep -q "$PUBKEYFPRINT" ; then
 	ssh-add $SSH_KEYFILE
 fi
 
-if grep 'fuse.sshfs' /proc/mounts | grep ~/sshfs | grep -q "${SSH_USER}@${SSH_IP}" ; then
+if grep 'fuse.sshfs' /proc/mounts | grep "${HOME}/sshfs " | grep -q "${SSH_USER}@${SSH_IP}:/" ; then
 	# we're already mounted, nothing to do
 	echo "INFO: Already mounted"
 	CLOUD_OUTPUT=""
-elif grep 'fuse.sshfs' /proc/mounts | grep -q ~/sshfs ; then
+elif grep 'fuse.sshfs' /proc/mounts | grep -q "${HOME}/sshfs " ; then
 	echo 'WARNING: Something else is already mounted on mount point "~/sshfs". Attempting umount.'
 	if fusermount -u ~/sshfs ; then
-	       echo 'INFO: umount succeeded.'
+	       echo 'INFO: Umount succeeded.'
 	else
 		echo 'ERROR: Could not umount "~/sshfs". Aborting.'
 		exit 1
@@ -43,36 +44,42 @@ elif grep 'fuse.sshfs' /proc/mounts | grep -q ~/sshfs ; then
 fi
 if [ -n "$CLOUD_OUTPUT" ]; then
 	if ! eval $($CLOUD_OUTPUT) ; then
-		echo 'ERROR: Could not mount sshfs. Aborting.'
+		echo 'ERROR: Could not mount "~/sshfs". Aborting.'
 		exit 1
+	else
+		echo 'INFO: Mount succeeded.'
 	fi
 fi
-
 # "source" remote config file
 eval $(ssh -l $SSH_USER -p $SSH_PORT -i $SSH_KEYFILE $SSH_OPTIONS $SSH_IP 'test -s ~/.gcs-x2go && cat ~/.gcs-x2go | grep -v "^ *#"' 2>/dev/null)
-FREEZER_STATE=$(ssh -l $SSH_USER -p $SSH_PORT -i $SSH_KEYFILE $SSH_OPTIONS $SSH_IP '(test -s ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'.tar.xz || test -s ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'-backup.tar.xz) 2>/dev/null || echo "EMPTYFREEZER"')
+if ! [ -x ~/sshfs/${SERVERNAME}/bin/bash ]; then
+	FREEZER_STATE=$(ssh -l $SSH_USER -p $SSH_PORT -i $SSH_KEYFILE $SSH_OPTIONS $SSH_IP '(test -s ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'.tar.xz || test -s ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'-backup.tar.xz) 2>/dev/null || echo "EMPTYFREEZER"')
 
-if [ "$FREEZER_STATE" == "EMPTYFREEZER" ] ; then
-	echo 'ERROR: No frozen server image found. Aborting.'
-	exit 1
+	if [ "$FREEZER_STATE" == "EMPTYFREEZER" ] ; then
+		echo 'ERROR: No frozen server image found. Aborting.'
+		exit 1
+	else
+		echo 'INFO: Frozen server image found.'
+	fi
+	# if both our frozen images exist, do nothing, else try to hardlink the first with the second; if that fails, the second with the first
+	ssh -l $SSH_USER -p $SSH_PORT -i $SSH_KEYFILE $SSH_OPTIONS $SSH_IP '! (test -s ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'.tar.xz && test -s ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'-backup.tar.xz) && sudo ln ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'-backup.tar.xz ~/'"${SERVERNAME}"'-home/'${SERVERNAME}'.tar.xz ' 2>/dev/null
+	ssh -l $SSH_USER -p $SSH_PORT -i $SSH_KEYFILE $SSH_OPTIONS $SSH_IP '! (test -s ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'.tar.xz && test -s ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'-backup.tar.xz) && sudo ln ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'.tar.xz ~/'"${SERVERNAME}"'-home/'${SERVERNAME}'-backup.tar.xz' 2>/dev/null
+
+	# Start thawing the server
+	echo 'INFO: Attempting to thaw the server.'
+	ssh -t -l $SSH_USER -p $SSH_PORT -i $SSH_KEYFILE $SSH_OPTIONS $SSH_IP '~/gopath/bin/thawserver 2>&1 | tee ~/thawserver-log' 2>/dev/null || exit 1
+else
+	echo 'INFO: Server directory already present, attempting to use that'
 fi
-# if both our frozen images exist, do nothing, else try to hardlink the first with the second; if that fails, the second with the first
-ssh -l $SSH_USER -p $SSH_PORT -i $SSH_KEYFILE $SSH_OPTIONS $SSH_IP '! (test -s ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'.tar.xz && test -s ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'-backup.tar.xz) && sudo ln ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'-backup.tar.xz ~/'"${SERVERNAME}"'-home/'${SERVERNAME}'.tar.xz ' 2>/dev/null
-ssh -l $SSH_USER -p $SSH_PORT -i $SSH_KEYFILE $SSH_OPTIONS $SSH_IP '! (test -s ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'.tar.xz && test -s ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'-backup.tar.xz) && sudo ln ~/'"${SERVERNAME}"'-home/'"${SERVERNAME}"'.tar.xz ~/'"${SERVERNAME}"'-home/'${SERVERNAME}'-backup.tar.xz' 2>/dev/null
-
-# Start thawing the server
-echo 'INFO: Attempting to thaw the server.'
-ssh -t -l $SSH_USER -p $SSH_PORT -i $SSH_KEYFILE $SSH_OPTIONS $SSH_IP '~/gopath/bin/thawserver 2>&1 | tee ~/thawserver-log' 2>/dev/null || exit 1
-
 # Add PUBKEY to default user, if not already present
 PUBKEY=$(cat ${SSH_KEYFILE}.pub)
-if ! grep -q "$PUBKEY" ~/sshfs/${SERVERNAME}-home/${USERNAME}/.ssh/authorized_keys ; then
-	echo "$PUBKEY">> ~/sshfs/${SERVERNAME}-home/${USERNAME}/.ssh/authorized_keys
+if ! grep -q "$PUBKEY" ~/sshfs/${SERVERNAME}/home/${USERNAME}/.ssh/authorized_keys ; then
+	echo "$PUBKEY">> ~/sshfs/${SERVERNAME}/home/${USERNAME}/.ssh/authorized_keys
 fi
 
 REMOTEUSERLIST=$(echo -e "${USERNAME}\n$(ssh -l $SSH_USER -p $SSH_PORT -i $SSH_KEYFILE $SSH_OPTIONS $SSH_IP 'sudo chroot /'"${SERVERNAME}"' getent group users' 2>/dev/null | awk -F ':' '{ print $4 }' | tr ',' '\n')" | sort -u | tr '\n' ' ')
 
-echo 'INFO: Creating X2Go Session Config file in "~/sshfs/google-cloud-x2go-server/clientside/gcs-sessions".'
+echo 'INFO: Creating X2Go Session Config file in "~/.x2goclient/gcs-sessions".'
 
 mkdir -p ~/.x2goclient
 # create empty file
@@ -89,16 +96,16 @@ for REMOTEUSER in $REMOTEUSERLIST; do
 	sed 	-e "s/TIMESTAMP/$TIMESTAMP_HEADER/" \
 		-e "s/USERNAME/$REMOTEUSER/g" \
 		-e "s/AUTOLOGINSTATE/$AUTOLOGINSTATE/g" \
-		-e "s/AUTOLOGINSTATE/$AUTOLOGINKEY/g" \
+		-e "s#AUTOLOGINKEY#$AUTOLOGINKEY#g" \
 		-e "s/PROXYIP/$SSH_IP/" \
 		-e "s/PROXYPORT/$SSH_PORT/" \
 		-e "s/GCLOUDACCOUNT/$SSH_USER/" \
 		-e "s#GCLOUDKEYFILE#$SSH_KEYFILE#" \
-		~/sshfs/google-cloud-x2go-server/clientside/gcs-session-template >> ~/.x2goclient/gcs-sessions
+		~/sshfs/home/${SSH_USER}/google-cloud-x2go-server/clientside/gcs-session-template >> ~/.x2goclient/gcs-sessions
 done
 
 echo 'INFO: Starting X2GoClient.'
-x2goclient --session-conf=~/sshfs/google-cloud-x2go-server/clientside/gcs-sessions &
+x2goclient --session-conf=~/.x2goclient/gcs-sessions &
 
 # Spawn the server
 ssh -l $SSH_USER -p $SSH_PORT -i $SSH_KEYFILE $SSH_OPTIONS $SSH_IP '~/gopath/bin/startserver-google-jumphost' 2>/dev/null
